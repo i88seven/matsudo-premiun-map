68f89bc44ad97ab99f3c1cfeaf2420f8db34c7f1
i88seven <i88.jelounjem@gmail.com>
2021-12-20 00:21:03 +0900

    Add matsudo map by leaflet

--- src/components/pages/HomePage.tsx
+++ src/components/pages/HomePage.tsx
@@ -1,12 +1,24 @@
 import React from "react";
-import GenericTemplate from "../templates/GenericTemplate";
+import { MapContainer, TileLayer, } from "react-leaflet";
+import { LatLng } from "leaflet";
+import "leaflet/dist/leaflet.css";
+
+import "App.css";
 
 const HomePage: React.VFC = () => {
+  // デフォルトは新八柱駅
+  let position = new LatLng(35.791714531276135, 139.93828231114674);
+
   return (
-    <GenericTemplate title="トップページ">
-      <>トップページ内容</>
-    </GenericTemplate>
+    <div className="App">
+      <MapContainer center={position} zoom={18}>
+        <TileLayer
+          attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+      </MapContainer>
+    </div>
   );
-};
+}
 
 export default HomePage;
